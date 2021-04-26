43bafe6c5fabe9d0ad5ac97e45232c4ad07c527c
The Kingdom Of God Is Within You <The_Kingdom_Of_God_Is_Within_You@protonmail.com>
2021-04-26 21:51:01 +0100

    Revert ":construction: development path"
    This reverts commit 5770cb98eb700db169c56c686e9fd96806539045.

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ function IndexPage (props) {
     </Fragment>
   );
 }
-IndexPage.getServerSideProps = async({ params = {}, isServer, store}) => {
+IndexPage.getInitialProps = async({ params = {}, isServer, store}) => {
   // const productsData = await getClient(preview).fetch(query);
   // Fetch today NASA APOD
   await store.execSagaTasks(isServer, dispatch => {
